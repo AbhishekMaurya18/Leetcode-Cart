d516dd35d215635a3d6a40a4121096924b9d393c
ABHISHEK Kr. MAURYA <38384913+AbhishekMaurya18@users.noreply.github.com>
2021-10-23 18:25:03 +0530

    Time: 60 ms (85.50%), Space: 10.4 MB (85.80%) - LeetHub

---
+++ coin-change/coin-change.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        int t[n+1][amount+1];
+        
+        //initilization
+        for(int i=0;i<=n;i++){
+            t[i][0]=0;
+        }
+        for(int j=0;j<=amount;j++){
+            t[0][j]=INT_MAX-1;
+            
+        }
+        //fill second row with respective coin numbers
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0){
+                t[1][j]=j/coins[0];
+            }
+            else{
+                t[1][j]=INT_MAX-1;
+            }
+            
+        }
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    t[i][j]=min(1+t[i][j-coins[i-1]],t[i-1][j]);
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][amount]>10000?-1:t[n][amount];
+    }
+};
