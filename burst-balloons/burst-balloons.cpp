527cd80c3e011a761c14aaa918fda8a867154f4c
ABHISHEK Kr. MAURYA <38384913+AbhishekMaurya18@users.noreply.github.com>
2021-11-26 18:30:25 +0530

    Time: 736 ms (35.91%), Space: 10.3 MB (10.63%) - LeetHub

--- burst-balloons/burst-balloons.cpp
+++ burst-balloons/burst-balloons.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    vector<vector<int>> t;
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         int n=nums.size();
-        vector<vector<int>> t(n,vector<int> (n,-1));
-        return mcm(nums,1,n-1,t);
+        t= vector<vector<int>>(n,vector<int> (n,-1));
+        return mcm(nums,1,n-1);
         
     }
     
-    int mcm(vector<int> &nums,int i,int j,vector<vector<int>>&t){
+    int mcm(vector<int> &nums,int i,int j){
         if(i>=j){
             return 0;
         }
@@ -18,7 +19,7 @@ public:
         }
         int mx=0;
         for(int k=i;k<j;k++){
-            int temp_ans=mcm(nums,i,k,t)+mcm(nums,k+1,j,t)+nums[i-1]*nums[k]*nums[j];
+            int temp_ans=mcm(nums,i,k)+mcm(nums,k+1,j)+nums[i-1]*nums[k]*nums[j];
             mx=max(mx,temp_ans);
         }
         return t[i][j]=mx;
